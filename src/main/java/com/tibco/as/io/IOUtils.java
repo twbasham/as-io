d6269179350bd79edfb75e194fadd5ba4a65e0ea
jruaux <jruaux AT tibco DOT com>
2014-09-15 20:37:00 -0700

    added test util methods

--- src/main/java/com/tibco/as/io/IOUtils.java
+++ src/main/java/com/tibco/as/io/IOUtils.java
@@ -1,8 +1,12 @@
 package com.tibco.as.io;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.text.MessageFormat;
 
 public class IOUtils {
 
@@ -30,8 +34,9 @@ public class IOUtils {
 		}
 		return filename.substring(0, position);
 	}
-	
-	public static void copy(InputStream inp, OutputStream out) throws IOException {
+
+	public static void copy(InputStream inp, OutputStream out)
+			throws IOException {
 		byte[] buff = new byte[4096];
 		int count;
 		while ((count = inp.read(buff)) != -1) {
@@ -40,4 +45,53 @@ public class IOUtils {
 			}
 		}
 	}
+
+	public static File copy(String resource, File dir) throws IOException {
+		return copy(resource, dir, resource);
+	}
+
+	public static InputStream getResourceAsStream(String resource) {
+		return ClassLoader.getSystemClassLoader().getResourceAsStream(resource);
+	}
+
+	public static File createTempDirectory() throws IOException {
+		File dir = File.createTempFile(IOUtils.class.getName(),
+				String.valueOf(System.currentTimeMillis()));
+		if (!dir.delete()) {
+			throw new IOException(MessageFormat.format(
+					"Could not delete temp file: {0}", dir.getAbsolutePath()));
+		}
+		if (!dir.mkdir()) {
+			throw new IOException(MessageFormat.format(
+					"Could not create temp directory: {0}",
+					dir.getAbsolutePath()));
+		}
+		return dir;
+	}
+
+	public static File copy(String resource, File dir, String filename)
+			throws IOException {
+		File file = new File(dir, filename);
+		copyToFile(resource, file);
+		return file;
+	}
+
+	public static void copyToFile(String resource, File destination)
+			throws IOException {
+		OutputStream out = new FileOutputStream(destination);
+		try {
+			InputStream in = getResourceAsStream(resource);
+			if (in == null) {
+				throw new FileNotFoundException(resource);
+			}
+			try {
+				IOUtils.copy(in, out);
+			} finally {
+				in.close();
+			}
+		} finally {
+			out.close();
+		}
+	}
+
 }
